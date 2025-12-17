8b5f91d14459e0276746a656922a0a73b9cba18e
Satyam <sk.m416.6x@gmail.com>
2025-12-17 22:13:43 +0530

    Contain Duplicates part 2

--- Day01_Arrays/ContainsDuplicate.cs
+++ Day01_Arrays/ContainsDuplicate.cs
@@ -14,4 +14,17 @@ public class Solution
 
         return false;
     }
+    public bool ContainsDuplicate(int[] nums)
+    {
+        Array.Sort(nums);
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+
+        return false;
+    }
+
 }
